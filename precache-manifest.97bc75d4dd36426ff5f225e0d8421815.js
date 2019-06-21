a27928f86b7ae875bea4a2c94252b2d8148b2ce3
Naufal Rabbani <naufal@Naufals-MacBook-Pro.local>
2019-06-21 10:45:50 +0700

    Updates

---
+++ precache-manifest.97bc75d4dd36426ff5f225e0d8421815.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f344fbd170c4b6262ca2ec756e9c8e5",
+    "url": "/uas-jst/index.html"
+  },
+  {
+    "revision": "ef5b4283c4bbb54bb2ae",
+    "url": "/uas-jst/static/css/main.8ebf5459.chunk.css"
+  },
+  {
+    "revision": "c5074f177f3d0806ceb8",
+    "url": "/uas-jst/static/js/2.33b05a62.chunk.js"
+  },
+  {
+    "revision": "ef5b4283c4bbb54bb2ae",
+    "url": "/uas-jst/static/js/main.e576aeb0.chunk.js"
+  },
+  {
+    "revision": "6f2bf1b388114347a8d1",
+    "url": "/uas-jst/static/js/runtime~main.57f1175e.js"
+  }
+]);
